ba3248616204e0b5d986d39c7c2b59f2c02de892
AndreyGatsuk <148007543+AndreyGatsuk@users.noreply.github.com>
2023-10-17 22:59:40 +0300

    Update UserRequestProcessor.java

--- Lab 1/task 2/UserRequestProcessor.java
+++ Lab 1/task 2/UserRequestProcessor.java
@@ -3,26 +3,47 @@ import java.util.concurrent.*;
 
 public class UserRequestProcessor {
     public static void main(String[] args) throws Exception {
+        // Создаем объект класса Calculator для вычисления квадрата числа
         Calculator squareCalculator = new Calculator();
+
+        // Создаем объект Scanner для ввода чисел пользователем
         Scanner in = new Scanner(System.in);
+
+        // Запрашиваем число у пользователя
         System.out.print("Вычислить квадрат для числа ");
         int x = in.nextInt();
-        Future future = squareCalculator.calculate(x);
+
+        // Вычисляем квадрат числа в отдельном потоке и получаем объект Future для получения результата
+        Future<Integer> future = squareCalculator.calculate(x);
+
+        // Запрашиваем еще одно число у пользователя
         int x1 = in.nextInt();
-        Future future1 = squareCalculator.calculate(x1);
+
+        // Вычисляем квадрат второго числа в отдельном потоке и получаем объект Future для получения результата
+        Future<Integer> future1 = squareCalculator.calculate(x1);
+
+        // Ожидаем завершения вычислений и выводим результаты
         System.out.println("Квадрат числа: " + future.get());
         System.out.println("Квадрат числа: " + future1.get());
     }
 }
 
+// Класс Calculator представляет собой вычислитель, который вычисляет квадрат числа в отдельном потоке
 class Calculator {
+    // Создаем пул потоков с фиксированным числом потоков (в данном случае, 2)
     private ExecutorService executor = Executors.newFixedThreadPool(2);
 
-    public Future calculate(Integer input) {
+    // Метод calculate принимает число, вычисляет его квадрат асинхронно и возвращает Future с результатом
+    public Future<Integer> calculate(Integer input) {
         return executor.submit(() -> {
+            // Генерируем случайную задержку от 1 до 5 секунд (в миллисекундах)
             int time = (int) ((Math.random() * 4000) + 1000);
             System.out.println("Время задержки: " + time + " миллисекунд");
+
+            // Имитируем обработку запроса с задержкой
             Thread.sleep(time);
+
+            // Вычисляем квадрат числа
             return input * input;
         });
     }
