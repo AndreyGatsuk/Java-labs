a3c34281a398baff445b0b2583e7737e47658861
AndreyGatsuk <148007543+AndreyGatsuk@users.noreply.github.com>
2023-10-17 22:47:42 +0300

    Update FirstMethod.java

--- Lab 1/task 1/FirstMethod.java
+++ Lab 1/task 1/FirstMethod.java
@@ -1,23 +1,38 @@
 import java.util.concurrent.TimeUnit;
 
 public class FirstMethod {
+
     public static void main(String[] args) throws InterruptedException {
+        // Измеряем используемую память до создания массива
         long usedBytes = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+
+        // Создаем массив из 10000 элементов и заполняем его значениями от 0 до 9999
         int[] array;
         array = new int[10000];
         for (int i = 0; i < array.length; i++) {
             array[i] = i;
         }
+
+        // Засекаем время начала выполнения поиска максимального элемента в массиве
         long start = System.currentTimeMillis();
+
+        // Ищем максимальный элемент в массиве
         int max = array[0];
         for (int i = 0; i < array.length; i++) {
-            if (i > max)
-                max = i;
+            if (array[i] > max) {
+                max = array[i];
+            }
+            // Задержка в 1 мс для имитации операции и замера времени выполнения
             TimeUnit.MILLISECONDS.sleep(1);
         }
+
+        // Засекаем время окончания выполнения и вычисляем общее время выполнения
         long finish = System.currentTimeMillis();
         long time = finish - start;
-        System.out.println("Задействовано " + usedBytes + " байт\n" + "Время выполнения: " + time + " милисекунды\n"
+
+        // Выводим информацию о памяти, времени выполнения и найденном максимальном элементе
+        System.out.println("Задействовано " + usedBytes + " байт\n"
+                + "Время выполнения: " + time + " миллисекунд\n"
                 + "Максимальный элемент массива: " + max);
     }
 }
